22db7c467dccad53c902ca6c743be6afac967f60
Wolfie <Wolfkid200444@gmail.com>
2021-08-01 19:21:15 -0400

    Made this cooler

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Search someone roblox user with powercord
-
-//powercord imports
 const { Plugin } = require("powercord/entities");
 const { get } = require("powercord/http");
 
@@ -12,52 +9,51 @@ module.exports = class Roblox extends Plugin {
     powercord.api.commands.registerCommand({
       name: "roblox",
       description: "Search for a roblox user",
-      usage: "<username>",
-      permission: "",
-      execute: (args, channel, user) => {
+      usage: "{c} <username>",
+      execute: async (args, channel) => {
         // try catch
         try {
           // Get the user
-          get(
+          const { body } = await get(
             `https://api.roblox.com/users/get-by-username?username=${args.username}`
-          )
-            .then(
-              (data) => {
-                  console.log(data);
-                // Check if the user exists
-                if (data.Data.Id != null) {
-                  // Send the user
-                  return {
-                    username: data.Data.Username,
-                    avatar_url: `https://roblox.com/Thumbs/BCOverlay.ashx?username=${data.Data.Username}`,
-                    result: {
-                      type: "rich",
-                      fields: [
-                        {
-                            name: "STATS",
-                            value: [
-                                `Username: ${data.Data.Username}`
-                            ].join('\n'),
-                            inline: true
-                        },
-                    ]
-                    },
-                  };
-                } else {
-                  // Send the user
-                  return {
-                    username: "Roblox",
-                    avatar_url: "https://i.imgur.com/uRpvasp.png",
-                    result: "No user Found",
-                  };
-                }
-              }
-              // If the user doesn't exist
-            )
-            .catch((err) => {
-              // Send the user
-              channel.send("User not found");
-            });
+          );
+          
+          // Check if the user exists
+          if (body.Data.Id != null) {
+
+            const data = await get(
+              `https://api.roblox.com/ownership/hasasset?userId=${body.Data.id}&assetId=102611803`
+            );
+            const Verifiedcheck = data.body
+
+            // Send the user
+            return {
+              username: body.Data.Username,
+              avatar_url: `https://roblox.com/Thumbs/BCOverlay.ashx?username=${body.Data.Username}`,
+              result: {
+                type: "rich",
+                fields: [
+                  {
+                    name: "STATS",
+                    value: [
+                      `Username: ${body.Data.Username}`,
+                      `ID: ${body.Data.Id}`,
+                      `Verified: ${Verifiedcheck ? 'Yes' : 'NOPE'}`,
+                      `[Profile Link](https://web.roblox.com/users/${body.Data.Id}/profile "Nothing SUS")`
+                    ].join("\n"),
+                    inline: true,
+                  },
+                ],
+              },
+            };
+          } else {
+            // Send the user
+            return {
+              username: "Roblox",
+              avatar_url: "https://i.imgur.com/uRpvasp.png",
+              result: "No user Found",
+            };
+          }
         } catch (err) {
           // Send the user
           channel.send("Error");
